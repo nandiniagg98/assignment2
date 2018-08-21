e1ccbeca050b7e52033510a22d74ee9a999a8bda
nandini <nandiniagg98@gmail.com>
2018-08-21 18:45:06 +0530

    My assignment

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+{
+int a,b,c;
+System.out.println("enter two numbers");
+Scanner sc= new Scanner(System.in);
+a= sc.nextInt();
+b= sc.nextInt();
+c= a + b;
+System.out.println("sum of the two numbers is=" + c);
+}
+}
+
+
